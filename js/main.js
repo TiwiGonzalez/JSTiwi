746290c9c6302688f8f8b0eb8a55b5ef49778f64
TiwiGonzalez <tiwikakigonzalez2010@gmail.com>
2022-09-07 00:48:22 -0300

    Primer entrega del proyecto final

--- js/main.js
+++ js/main.js
@@ -9,9 +9,8 @@ class skin{
     }
     //Uso del "toString" para reciclar lineas de codigo y esté más optimizado
     toString(){
-        return"Personaje: " + this.personaje + " | Nombre del skin: " + this.nombre + " | Bundle: " + this.bundle + " | Precio: " + this.precio;
+        return"Personaje: " + this.personaje + " | Nombre del skin: " + this.nombre + " | Bundle: " + this.bundle + " | Precio: " + this.precio + " RP";
     }
-
     /*
     getName(){
         return this.personaje;
@@ -21,35 +20,54 @@ class skin{
         return this.nombre;
     }*/
 }
+function menorAMayor(a,b) {
+    return (a.precio) - (b.precio);
+}
+function mayorAMenor(a,b) {
+    return ((a.precio) - (b.precio)) * -1;
+}
+
+function alfabeticamente(a,b) {
+    return a.personaje.localeCompare(b.personaje);
+}
+
+function porNombre(nombrePj,array){
+    const skins = [];
+    for(let champion of array){
+        if(champion.personaje == nombrePj){
+            skins.push(champion);
+        }
+    }
+    return skins;
+}
 
 //Bundle Valquirias de acero
 const valquirias = [];
-valquirias.push(new skin ("Titan Acorazado", "Nasus", "1350 RP", "Valquirias de acero"))
-valquirias.push(new skin ("Halo Cibernético", "Janna", "1350 RP", "Valquirias de acero"))
-valquirias.push(new skin ("Comandante de ataque", "Camille", "1350 RP", "Valquirias de acero"))
-valquirias.push(new skin ("Paladin de ataque", "Lucian", "1350 RP", "Valquirias de acero"))
-
+valquirias.push(new skin ("Titan Acorazado", "Nasus", 1350, "Valquirias de acero"))
+valquirias.push(new skin ("Halo Cibernético", "Janna", 950, "Valquirias de acero"))
+valquirias.push(new skin ("Comandante de ataque", "Camille", 1000, "Valquirias de acero"))
+valquirias.push(new skin ("Paladin de ataque", "Lucian", 365, "Valquirias de acero"))
 
 //Bundle Guardianes estelares
 const guardianesEstelares = [];
-guardianesEstelares.push(new skin ("Guardian Estelar", "Kai'sa", "1820 RP", "Guardianes Estelares"))
-guardianesEstelares.push(new skin ("Guardian Estelar", "Ahri", "1820 RP", "Guardianes Estelares"))
-guardianesEstelares.push(new skin ("Guardian Estelar", "Miss Fortune", "1820 RP", "Guardianes Estelares"))
-guardianesEstelares.push(new skin ("Guardian Estelar", "Ekko", "1820 RP", "Guardianes Estelares"))
+guardianesEstelares.push(new skin ("Guardian Estelar", "Kai'sa", 450 , "Guardianes Estelares"))
+guardianesEstelares.push(new skin ("Guardian Estelar", "Ahri", 920 , "Guardianes Estelares"))
+guardianesEstelares.push(new skin ("Guardian Estelar", "Miss Fortune", 1420 , "Guardianes Estelares"))
+guardianesEstelares.push(new skin ("Guardian Estelar", "Ekko", 1230 , "Guardianes Estelares"))
 
 //Bundle Campeonato 2019
 const campeonato2019 = [];
-campeonato2019.push(new skin ("Campeonato", "Riven", "975 RP", "Campeonato 2019"))
-campeonato2019.push(new skin ("Campeonato", "Zed", "975 RP", "Campeonato 2019"))
-campeonato2019.push(new skin ("Campeonato", "Shyvanna", "975 RP", "Campeonato 2019"))
-campeonato2019.push(new skin ("Campeonato", "Tresh", "975 RP", "Campeonato 2019"))
+campeonato2019.push(new skin ("Campeonato", "Riven", 3575 , "Campeonato 2019"))
+campeonato2019.push(new skin ("Campeonato", "Zed", 1275 , "Campeonato 2019"))
+campeonato2019.push(new skin ("Campeonato", "Shyvanna", 675 , "Campeonato 2019"))
+campeonato2019.push(new skin ("Campeonato", "Tresh",565 , "Campeonato 2019"))
 
-
+const todosPjs = [].concat(valquirias).concat(guardianesEstelares).concat(campeonato2019);
 //Selector de grupos o bundles de cada skins
 
 function bundleSelecter() {
     let bundles = parseInt(prompt(
-        "Elegí un bundle \n 1-Valquirias de acero \n 2-Guardianes Estelares \n 3-Campeonato 2019"
+        "Elegí un bundle \n 1-Valquirias de acero \n 2-Guardianes Estelares \n 3-Campeonato 2019 \n 4-TodosLosPersonajes"
     ))
 
     if (bundles == "1") {
@@ -57,22 +75,31 @@ function bundleSelecter() {
         for (let champion of valquirias) {
             console.log(champion.toString());
         }
-        let skins = alert("1-Nasus Titan Acorazado \n2-Janna Halo Cibernético \n3-Camille Comandante de ataque \n4-Lucian Paladin de ataque")
+        console.log(valquirias.sort(menorAMayor));
     }
 
     else if (bundles == "2") {
         for (let champion1 of guardianesEstelares) {
             console.log(champion1.toString());
         }
-        let skins = alert("1-Kai'sa Guardian Estelar \n2-Ahri Guardian Estelar \n3-Miss Fortune Guardian Estelar \n4-Ekko Guardian Estelar")
+        console.log(guardianesEstelares.sort(menorAMayor))
     }
 
     else if (bundles == "3") {
         for (let champion2 of campeonato2019){
             console.log(champion2.toString());
         }
-        let skins = alert("1-Riven Campeonato \n2-Zed Campeonato \n3-Shyvanna Campeonato \n4-Tresh Campeonato")
+        console.log(campeonato2019.sort(menorAMayor));
     }
+    else if (bundles == "4"){
+        console.log("todos ordenados Mayor a menor:");
+        console.log(todosPjs.sort(mayorAMenor));
+        console.log("todos ordenados Menor a mayor:");
+        console.log(todosPjs.sort(menorAMayor));
+        let nombre = prompt("Elija nombre de personaje")
+        console.log(porNombre(nombre,todosPjs))
+        
+    }   
     
     else{
         bundleSelecter();
